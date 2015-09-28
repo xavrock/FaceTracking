6c5579d7c207a9f5e5b49a087a71bbefe99cd9ba
Xavier Roche <xavier.roche@thalesgroup.com>
2015-09-28 23:57:46 +0200

    test
    Signed-off-by: Xavier Roche <xavier.roche@thalesgroup.com>

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,9 @@
 #include "Display.h"
 
 #include "opencv2/opencv.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 /**
  * Main class of project FaceTracker
@@ -35,7 +37,24 @@ int main(int argc, char **argv) {
 	disp = Display::getInstance();
 	
 	while(1){usleep(1000);}*/
-	VideoCapture 			capture;
+	
+	string cascadeName = "/usr/share/haarcascades/haarcascade_frontalface_alt2.xml";
+	
+
+    bool tryflip = false;
+    
+    CascadeClassifier cascade;
+    double scale = 1.4;
+    
+    if( !cascade.load( cascadeName ) )
+    {
+        cerr << "ERROR: Could not load classifier cascade" << endl;
+        return -1;
+    }
+    
+    
+	
+	VideoCapture capture;
 	capture.open(0);
 	
 	
@@ -46,6 +65,7 @@ int main(int argc, char **argv) {
 		Mat grayImage = Mat(Size(capture_width,capture_height),CV_8UC1);
 		Mat bgrImage = Mat(Size(capture_width,capture_height),CV_8UC3);
 	
+	cvNamedWindow( "Display FaceTracker", 1 );
 	
 	while(1)
 	{
