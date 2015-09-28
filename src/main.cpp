35d2d0f9d849ce72e4aea32a6ea39c569dc917ee
Xavier Roche <xavier.roche@thalesgroup.com>
2015-09-29 00:51:47 +0200

    test
    Signed-off-by: Xavier Roche <xavier.roche@thalesgroup.com>

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,15 @@ void detectAndDraw( Mat img, CascadeClassifier cascade, double scale )
         //|CV_HAAR_SCALE_IMAGE
         ,
         Size(30, 30) );
+        
+        if(faces.size() > 0 )
+        {
+			printf("Detected")
+		}
+		else
+		{
+			facedetected = false;
+		}
 	}
 }
 
